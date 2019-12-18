1ea01d6be3a6f82f5841b5810be458a2bc97f73b
Andreas Patsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2019-12-18 11:16:28 +0200

    updates

--- js/profiles/company.js
+++ js/profiles/company.js
@@ -1,1 +1,104 @@
 import{ formToJSON } from '../utils/bind.js';
+
+//constant fields
+const homeArea = document.querySelector("#homeArea"),
+
+      home = document.querySelector("#home"),
+
+      settings = document.querySelector("#settings"),
+
+      settingsForm = document.querySelector("#settingsForm"),
+
+      settingsArea = document.querySelector("#settingsArea"),
+
+      deleteMyProfile = document.querySelector("#deleteMyProfile");
+
+
+
+//tabs
+home.addEventListener("click", () => {
+
+    homeArea.style.display = "block";
+
+    settingsArea.style.display = "none";
+});
+
+settings.addEventListener("click", () => {
+
+    homeArea.style.display = "none";
+
+    settingsArea.style.display = "block";
+});
+
+// settings operations
+
+//profile values
+document.querySelectorAll(".profileImage").forEach(image => image.src = "images/pada.jpg");
+
+document.querySelector("#changePhotoLink").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    document.querySelector("#changePhoto").click();
+});
+
+//form
+settingsForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    let settingsData = formToJSON(settingsForm.elements);
+
+    if(document.querySelector("#ckb1").checked === true)
+        settingsData.workExperience = true;
+
+    else
+        settingsData.workExperience = false;
+
+        if(settingsData.mobilePhone.trim() === "" || settingsData.mobilePhone.length == 10){
+            
+            document.querySelector("#mobReq").style.display = "none";
+
+            document.querySelector("#loadingIma").style.display = "block";
+
+            console.log(settingsData);
+
+            let isMailUnique = true;
+
+            if(isMailUnique){
+                
+                setTimeout(() => {
+
+                    document.querySelector("#loadingIma").style.display = "none";
+                    
+                    document.querySelector("#mailSuccess").style.display = "block";
+                    
+                    setTimeout(() => location.reload(), 2000);
+
+                }, 3000);
+            }
+            
+            else{
+                setTimeout(() => {
+
+                    document.querySelector("#loadingIma").style.display = "none";
+                    
+                    document.querySelector("#mailFail").style.display = "block";
+
+                }, 3000);
+            }
+            
+             
+        }
+        else
+            document.querySelector("#mobReq").style.display = "block";
+});
+
+deleteMyProfile.addEventListener("click", () => {
+    
+    let confirmation = confirm("Are you sure you want to delete your profile?");
+
+    if(confirmation)
+        console.log("confirm");
+    else
+        console.log("No.")
+});
